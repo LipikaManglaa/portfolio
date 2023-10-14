1ec75f7507f04f6ea9ba4bc690fc68627c95e5c7
Lipika Mangla <131666154+LipikaManglaa@users.noreply.github.com>
2023-10-14 11:55:19 +1100

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout(props) {
   return (
     <>
     <Header/>
-    <main style={{background:'#F3E3D9;'}}>{props.children}</main>
+    <main>{props.children}</main>
     <Footer/>
     </>
   )
