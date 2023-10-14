3e3367432a019987d5acd31910142ed00235ebe0
Lipika Mangla <131666154+LipikaManglaa@users.noreply.github.com>
2023-10-14 12:04:59 +1100

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import './../styles/Header.css'
-import '../styles/About.css'
-import aboutImg from './../assets/profile.jpg'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faBars, faPhone } from '@fortawesome/free-solid-svg-icons'
 
@@ -33,20 +31,7 @@ export default function Header() {
                 </nav>
             </div>
         </header>
-        <section id="about">
-            <h3>About Me</h3>
-            <div class="about-inner">
-
-                <figure>
-           
-                </figure>
-                <div class="about-content pt-4">
-                    <h2>Full Stack Web Developer</h2>
-                    <p>Hi, My name is Lipika I've done my masters in computer science back in India after that I moved to Australia for better life .Then i was bust for settling life .After that, I was busy in home management And  in May 23 I've started Mern tack from Monash University coding bootcamp</p>
-                    
-                </div>
-            </div>
-        </section>
+     
       </>
   )
 }
